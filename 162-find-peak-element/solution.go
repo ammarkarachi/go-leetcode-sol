0a5fdd7b3fcbe5d541450ba5681f3f0177901947
ammarkarachi <github@ammarkarachi.com>
2025-02-23 00:08:50 -0800

    find peak element

--- 162-find-peak-element/solution.go
+++ 162-find-peak-element/solution.go
@@ -1,3 +1,19 @@
 package solution
 
 //https://leetcode.com/problems/find-peak-element/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
+
+func findPeakElement(nums []int) int {
+	return search(nums, 0, len(nums)-1)
+}
+
+func search(nums []int, l int, r int) int {
+	if l == r {
+		return l
+	}
+	mid := (l + r) / 2
+	if nums[mid] > nums[mid+1] {
+		return search(nums, l, mid)
+	} else {
+		return search(nums, mid+1, r)
+	}
+}
