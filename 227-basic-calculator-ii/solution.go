29d7e20ade510b065317bd1253502b1b9375217d
ammarkarachi <github@ammarkarachi.com>
2025-02-26 20:54:03 -0800

    basic calculator

--- 227-basic-calculator-ii/solution.go
+++ 227-basic-calculator-ii/solution.go
@@ -2,43 +2,34 @@ package solution
 
 func calculate(s string) int {
 	answer := 0
-	stack := []int{}
 	currentNumber := 0
+	lastNumber := 0
 	operand := '+'
 	for i, b := range s {
 		if isDigit(b) {
 			currentNumber = currentNumber*10 + toDigit(b)
-		} else if !isDigit(b) && !isSpace(b) && i == len(s)-1 {
-			if operand == '+' {
-				push(&stack, currentNumber)
-			}
-			if operand == '-' {
-				push(&stack, -currentNumber)
+		}
+		if (!isDigit(b) && !isSpace(b)) || i == len(s)-1 {
+			if operand == '+' || operand == '-' {
+				answer += lastNumber
+				if operand == '-' {
+					lastNumber = currentNumber * -1
+				} else {
+					lastNumber = currentNumber
+				}
 			}
 			if operand == '*' {
-				val := pop(&stack) * currentNumber
-				push(&stack, val)
+				lastNumber = lastNumber * currentNumber
 			}
 			if operand == '/' {
-				val := pop(&stack) / currentNumber
-				push(&stack, val)
+				lastNumber = lastNumber / currentNumber
 			}
 			currentNumber = 0
 			operand = b
 		}
 	}
-	for len(stack) > 0 {
-		answer += pop(&stack)
-	}
+	answer += lastNumber
 	return answer
-}
-func push(stack *[]int, val int) {
-	(*stack) = append([]int{val}, *stack...)
-}
-func pop(stack *[]int) int {
-	val := (*stack)[len(*stack)-1]
-	(*stack) = (*stack)[:len(*stack)-1]
-	return val
 }
 
 func isSpace(s rune) bool {
