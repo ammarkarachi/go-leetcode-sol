4b8ed6c964b2b718706e3310ed85ea4ddf567ae7
ammarkarachi <github@ammarkarachi.com>
2025-03-03 22:23:08 -0800

    all node distance k in binary tree

---
+++ 863-all-nodes-distance-k-in-binary-tree/solution.go
@@ -0,0 +1,57 @@
+package solution
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func distanceK(root *TreeNode, target *TreeNode, k int) []int {
+	graph := make(map[int][]int)
+	dfs(root, nil, &graph)
+
+	queue := [][2]int{
+		{target.Val, 0},
+	}
+	ans := []int{}
+	visited := make(map[int]bool)
+	visited[target.Val] = true
+	for len(queue) > 0 {
+		f := queue[0]
+		queue = queue[1:]
+
+		if f[1] == k {
+			ans = append(ans, f[0])
+			continue
+		}
+
+		n := graph[f[0]]
+
+		for _, node := range n {
+			if !visited[node] {
+				queue = append(queue, [2]int{node, f[1] + 1})
+			}
+		}
+
+	}
+	return ans
+}
+
+func dfs(node *TreeNode, parent *TreeNode, graph *map[int][]int) {
+	g := *graph
+
+	if node != nil && parent != nil {
+		g[node.Val] = append(g[node.Val], parent.Val)
+		g[parent.Val] = append(g[parent.Val], node.Val)
+	}
+
+	if node.Right != nil {
+		dfs(node.Right, node, graph)
+	}
+
+	if node.Left != nil {
+		dfs(node.Left, node, graph)
+	}
+
+	graph = &g
+}
