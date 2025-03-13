341a5a1ff43ea506644e45c4ca90b2262194a457
ammarkarachi <github@ammarkarachi.com>
2025-03-13 12:35:41 -0700

    remove element

---
+++ 27-remove-element/solution.go
@@ -0,0 +1,15 @@
+package solution
+
+func removeElement(nums []int, val int) int {
+	i := 0
+	n := len(nums)
+	for i < n {
+		if nums[i] == val {
+			nums[i] = nums[n-1]
+			n--
+		} else {
+			i++
+		}
+	}
+	return n
+}
