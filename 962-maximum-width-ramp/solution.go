aa83c1a58e9e28ed9a1795dd4ee44c3b0790247a
ammarkarachi <github@ammarkarachi.com>
2025-04-08 22:14:41 -0700

    some modifications

--- 962-maximum-width-ramp/solution.go
+++ 962-maximum-width-ramp/solution.go
@@ -4,13 +4,13 @@ func maxWidthRamp(nums []int) int {
 	stack := []int{}
 
 	for i, num := range nums {
-		if len(stack) > 0 || stack[0] > num {
+		if len(stack) == 0 || nums[stack[0]] > num {
 			stack = append([]int{i}, stack...)
 		}
 	}
 	maxWidth := 0
-	for i := len(nums) - 1; i >= 0; i++ {
-		for len(stack) > 0 && nums[stack[0]] >= nums[i] {
+	for i := (len(nums) - 1); i >= 0; i-- {
+		for len(stack) > 0 && nums[stack[0]] <= nums[i] {
 			maxWidth = max(maxWidth, i-stack[0])
 			stack = stack[1:]
 		}
